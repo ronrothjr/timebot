e17320fda4c257eb0f2a668ab5fe01dc5489ccee
Ron Roth Jr <ronrothjr@gmail.com>
2022-03-01 12:03:24 -0500

    add services to main

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@
 import os
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from db import DB
+from service import Service
+from project import Project
+from timecard import Timecard
+from day import Day
+from entry import Entry
 
 os.environ["DEFAULT_PROJECT_CODE"] = "DRG-403001"
 
@@ -21,36 +25,20 @@ MDScreen:
 
 class Example(MDApp):
     add_buttons = {
-        "Timecard": "file-table-box-multiple",
+        "Timecards": "file-table-box-multiple",
         "Project": "apps",
         "Entry": "clock-time-five-outline",
     }
 
     def build(self):
-        self.create_db()
+        self.services()
         return Builder.load_string(KV)
 
-    def create_db(self):
-        timebot_tables = {
-            'timecard': {
-                'begin_date': {'name': 'begin_date', 'type': 'TEXT', 'id': True},
-                'end_date': {'name': 'end_date', 'type': 'TEXT'}
-            },
-            'day': {
-                'timecard_begin_date': {'name': 'timecard_begin_date', 'type': 'TEXT', 'ref': 'timecard(begin_date)', 'trigger': 'CASCADE'},
-                'weekday': {'name': 'weekday', 'type': 'TEXT', 'id': True}
-            },
-            'project': {
-                'code': {'name': 'code', 'type': 'TEXT', 'id': True}
-            },
-            'entry': {
-                'day_weekday': {'name': 'day_weekday', 'type': 'TEXT', 'ref': 'day(weekday)', 'trigger': 'CASCADE'},
-                'begin': {'name': 'begin', 'type': 'TEXT'},
-                'end': {'name': 'end', 'type': 'TEXT'},
-                'project_code': {'name': 'project_code', 'type': 'TEXT', 'ref': 'project(code)', 'trigger': 'CASCADE'}
-            }
-        }
-        self.db = DB(db_name='app.db', tables=timebot_tables)
+    def services(self):
+        self.project = Service(Project)
+        self.timecard = Service(Timecard)
+        self.day = Service(Day)
+        self.entry = Service(Entry)
 
 
 Example().run()
