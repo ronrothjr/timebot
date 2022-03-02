bf6f7f16c360fee78e285d0aba6eb0eff9de162b
Ron Roth Jr <ronrothjr@gmail.com>
2022-03-02 12:24:14 -0500

    screen manager for loading splash screen fading to root screen

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-
-import os
+import os, sys
+from pathlib import Path
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
+from libs.baseclass.splash_screen import TimebotSplashScreen
+from libs.baseclass.root_screen import TimebotRootScreen
 from service import Service
 from project import Project
 from timecard import Timecard
@@ -10,29 +14,32 @@ from entry import Entry
 
 os.environ["DEFAULT_PROJECT_CODE"] = "DRG-403001"
 
-KV = """
-MDScreen:
-
-    MDToolbar:
-        title: 'Time Bot'
-        pos_hint: {'top': 1}
-
-    MDFloatingActionButtonSpeedDial:
-        data: app.add_buttons
-        rotation_root_button: True
-"""
+if getattr(sys, "frozen", False):  # bundle mode with PyInstaller
+    os.environ["TIMEBOT_ROOT"] = sys._MEIPASS
+else:
+    os.environ["TIMEBOT_ROOT"] = str(Path(__file__).parent)
 
 
-class Example(MDApp):
+KV_DIR = f"{os.environ['TIMEBOT_ROOT']}/libs/kv/"
+
+for kv_file in os.listdir(KV_DIR):
+    with open(os.path.join(KV_DIR, kv_file), encoding="utf-8") as kv:
+        Builder.load_string(kv.read())
+
+
+class MDTimebot(MDApp):
+
     add_buttons = {
-        "Timecards": "file-table-box-multiple",
-        "Project": "apps",
         "Entry": "clock-time-five-outline",
+        "Projects": "apps",
+        "Timecards": "file-table-box-multiple",
     }
 
-    def build(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.title = "Timebot"
+        self.icon = f"{os.environ['TIMEBOT_ROOT']}/assets/images/logo.png"
         self.services()
-        return Builder.load_string(KV)
 
     def services(self):
         self.project = Service(Project)
@@ -40,5 +47,62 @@ class Example(MDApp):
         self.day = Service(Day)
         self.entry = Service(Entry)
 
+    def callback(self, instance):
+        print(instance.icon)
 
-Example().run()
+    def build(self):
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.theme_style = "Dark"
+        FONT_PATH = f"{os.environ['TIMEBOT_ROOT']}/assets/fonts/"
+
+        self.theme_cls.font_styles.update(
+            {
+                "H1": [FONT_PATH + "RobotoCondensed-Light", 96, False, -1.5],
+                "H2": [FONT_PATH + "RobotoCondensed-Light", 60, False, -0.5],
+                "H3": [FONT_PATH + "Eczar-Regular", 48, False, 0],
+                "H4": [FONT_PATH + "RobotoCondensed-Regular", 34, False, 0.25],
+                "H5": [FONT_PATH + "RobotoCondensed-Regular", 24, False, 0],
+                "H6": [FONT_PATH + "RobotoCondensed-Bold", 20, False, 0.15],
+                "Subtitle1": [
+                    FONT_PATH + "RobotoCondensed-Regular",
+                    16,
+                    False,
+                    0.15,
+                ],
+                "Subtitle2": [
+                    FONT_PATH + "RobotoCondensed-Medium",
+                    14,
+                    False,
+                    0.1,
+                ],
+                "Body1": [FONT_PATH + "Eczar-Regular", 16, False, 0.5],
+                "Body2": [FONT_PATH + "RobotoCondensed-Light", 14, False, 0.25],
+                "Button": [FONT_PATH + "RobotoCondensed-Bold", 14, True, 1.25],
+                "Caption": [
+                    FONT_PATH + "RobotoCondensed-Regular",
+                    12,
+                    False,
+                    0.4,
+                ],
+                "Overline": [
+                    FONT_PATH + "RobotoCondensed-Regular",
+                    10,
+                    True,
+                    1.5,
+                ],
+                "Total": [FONT_PATH + "Eczar-SemiBold", 48, False, 0],
+            }
+        )
+        self.sm = ScreenManager(transition=FadeTransition())
+        self.sm.add_widget(TimebotSplashScreen(name="timebot splash screen"))
+        self.sm.add_widget(TimebotRootScreen(name="timebot root screen"))
+        self.sm.current = "timebot splash screen"
+        Clock.schedule_once(self.switch, 3)
+        return self.sm
+
+    def switch(self, dt):
+        print('switch')
+        self.sm.current = "timebot root screen"
+
+
+MDTimebot().run()
