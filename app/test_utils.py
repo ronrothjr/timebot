c8b0ce9aae8cdc6baf4230aeeff8b050b061ee82
Ron Roth Jr <ronrothjr@gmail.com>
2022-03-03 10:11:57 -0500

    generate tuples for data tables

---
+++ app/test_utils.py
@@ -0,0 +1,30 @@
+import unittest, datetime, os
+from timecard import Timecard
+from utils import Utils
+
+
+class TestUntils(unittest.TestCase):
+
+    def test_get_tuples_from_data(self):
+        today = datetime.datetime.now()
+        begin_date = str((today - datetime.timedelta(days=today.weekday() + 1)).date())
+        timecard_data = {
+            'days': {
+                'Monday': {
+                    'dayid': 0, 'begin_date': begin_date, 'weekday': 'Monday', 'entries': {
+                        0: {'dayid': 0, 'entryid': 0, 'begin': '0900', 'end': '1600', 'code': os.environ["DEFAULT_PROJECT_CODE"]}
+                    }
+                }
+            }
+        }
+        new_timecard = Timecard(begin_date, timecard_data)
+        timecard_dict = new_timecard.as_dict()
+        column_tuple = Utils.schema_dict_to_tuple('timecard')
+        self.assertEqual(column_tuple, [('Begin', 70), ('End', 70)])
+        data_tuple = Utils.data_to_tuple('timecard', [timecard_dict])
+        self.assertEqual(data_tuple, [('2022-02-27', '2022-03-05')])
+
+
+if __name__ == '__main__':
+    os.environ["DEFAULT_PROJECT_CODE"] = '403001'
+    unittest.main()
--- app/utils.py
+++ app/utils.py
@@ -20,26 +20,39 @@ class Utils:
     def get_schema():        
         tables = {
             'project': {
-                'code': {'name': 'code', 'type': 'TEXT', 'id': True, 'dp': 70}
+                'code': {'name': 'code', 'display': 'Code', 'type': 'TEXT', 'id': True, 'dp': 70}
             },
             'timecard': {
-                'begin_date': {'name': 'begin_date', 'type': 'TEXT', 'id': True, 'dp': 70},
-                'end_date': {'name': 'end_date', 'type': 'TEXT', 'dp': 70}
+                'begin_date': {'name': 'begin_date', 'display': 'Begin', 'type': 'TEXT', 'id': True, 'dp': 70},
+                'end_date': {'name': 'end_date', 'display': 'End', 'type': 'TEXT', 'dp': 70}
             },
             'day': {
                 'begin_date': {'name': 'begin_date', 'type': 'TEXT', 'dp': 70, 'ref': 'timecard(begin_date)', 'trigger': 'CASCADE'},
-                'weekday': {'name': 'weekday', 'type': 'TEXT', 'dp': 70}
+                'weekday': {'name': 'weekday', 'display': 'Weekday', 'type': 'TEXT', 'dp': 70}
             },
             'entry': {
                 'dayid': {'name': 'dayid', 'type': 'INTEGER', 'dp': 30, 'ref': 'day(dayid)', 'trigger': 'CASCADE'},
-                'begin': {'name': 'begin', 'type': 'TEXT', 'dp': 70},
-                'end': {'name': 'end', 'type': 'TEXT', 'dp': 70},
-                'code': {'name': 'code', 'type': 'TEXT', 'dp': 70, 'ref': 'project(code)', 'trigger': 'CASCADE'}
+                'begin': {'name': 'begin', 'display': 'Begin', 'type': 'TEXT', 'dp': 70},
+                'end': {'name': 'end', 'display': 'End', 'type': 'TEXT', 'dp': 70},
+                'code': {'name': 'code', 'display': 'Code', 'type': 'TEXT', 'dp': 70, 'ref': 'project(code)', 'trigger': 'CASCADE'}
             }
         }
         return {'db_name': 'app.db', 'tables': tables}
 
     @staticmethod
-    def schema_dict_to_tuple(schema: dict):
+    def schema_dict_to_tuple(table_name):
         columns = []
+        schema = Utils.get_schema()
+        for name, column in schema['tables'][table_name].items():
+            if column['display']:
+                columns.append((column['display'], column['dp']))
         return columns
+
+    @staticmethod
+    def data_to_tuple(table_name: str, data: list):
+        table = Utils.get_schema()['tables'][table_name]
+        rows = []
+        columns = [name for name, value in table.items() if value['display']]
+        for row_data in data:
+            rows.append(tuple([row_data[name] for name in columns]))
+        return rows
