5b078c635779a12ff5e80b9e48410738fdc23dd9
Ron Roth Jr <ronrothjr@gmail.com>
2022-03-08 12:03:14 -0500

    remove resize

--- app/main.py
+++ app/main.py
@@ -15,10 +15,10 @@ from timecard import Timecard
 from day import Day
 from entry import Entry
 from api import API
-from kivy.config import Config
-Config.set('graphics', 'width', '370')
-Config.set('graphics', 'height', '760')
-Config.write()
+# from kivy.config import Config
+# Config.set('graphics', 'width', '370')
+# Config.set('graphics', 'height', '760')
+# Config.write()
 
 os.environ["DEFAULT_PROJECT_CODE"] = "DRG-403001"
 
@@ -39,7 +39,7 @@ class MDTimebot(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        Window.size = (370, 760)
+        # Window.size = (370, 760)
         self.title = "Timebot"
         self.icon = f"{os.environ['TIMEBOT_ROOT']}/assets/images/logo.png"
         self.services()
